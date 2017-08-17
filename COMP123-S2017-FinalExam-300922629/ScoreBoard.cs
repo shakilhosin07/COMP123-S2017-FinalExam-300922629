836a191759393286f23bd763099b17b4f492e617
Zakir <Zakir@LAPTOP-GQEFR3RC>
2017-08-17 11:24:07 -0400

    Created all the private instance variables and properties for the scoreboard class

---
+++ COMP123-S2017-FinalExam-300922629/ScoreBoard.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+* Name: Shakil Hosin
+* Date: August 17th, 2017
+* Description: This is the ScoreBoard Class
+* Version: 0.1 - Added the OverWrite method
+*/
+namespace COMP123_S2017_FinalExam_300922629
+{
+    class ScoreBoard : PickHighestCardForm
+    {
+        //PRIVATE INSTANCE VARIABLES
+        private TextBox _finalScoreTextBox;
+        private int _score;
+        private TextBox _scoreTextBox;
+        private int _time;
+        private TextBox _timeTextBox;
+
+        //PUBLIC PROPERTIES
+        public TextBox FinalScoreTextBox
+        {
+            get
+            {
+                return this._finalScoreTextBox;
+            }
+            set
+            {
+                this._finalScoreTextBox = value;
+            }
+        }
+        public int Score
+        {
+            get
+            {
+                return this._score;
+                
+            }
+            set
+            {
+                this._score = value;
+            }
+        }
+        public TextBox ScoreTextBox
+        {
+            get
+            {
+                return this._scoreTextBox;
+            }
+            set
+            {
+                this._scoreTextBox = value;
+            }
+        }
+        public int Time
+        {
+            get
+            {
+                return this._time;
+            }
+            set
+            {
+                this._time = value;
+            }
+            
+        }
+        public TextBox TimeTextBox
+        {
+            get
+            {
+                return this._timeTextBox;
+            }
+            set
+            {
+                this._timeTextBox = value;
+            }
+        }
+        //CONSTRUCTORS
+    }
+}
